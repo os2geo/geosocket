6a70168ae1ff82dd8508f70ad1cb2a90d3dadc9c
Rune Tvilum <rune@addin.dk>
2017-02-22 11:49:48 +0100

    online

--- index.js
+++ index.js
@@ -486,13 +486,13 @@ emailTemplates(templatesDir, function (err, template) {
                 //console.log('authenticated', socket.decoded_token);
                 socket.emit('authenticated', { token: socket.token, profile: socket.decoded_token });
             }*/
-            sio.to('users').emit('users', sio.sockets.sockets.length);
+            sio.to('users').emit('users', {sockets:sio.sockets.sockets.length});
             socket.on('users', function () {
                 socket.join('users');
-                socket.emit('users', sio.sockets.sockets.length);
+                socket.emit('users', {sockets:sio.sockets.sockets.length});
             });
             socket.on('disconnect', function (data) {                
-                sio.to('users').emit('users', sio.sockets.sockets.length);
+                sio.to('users').emit('users', {sockets:sio.sockets.sockets.length});
             });
             //console.log(socket.decoded_token.email, 'connected');
             socket.on('queue', function (data) {
